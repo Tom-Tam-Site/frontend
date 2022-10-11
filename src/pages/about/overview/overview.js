50c75cd10503a3ced9727605bcf25806e92015a4
David Del Sol <daviddelsol1998@gmail.com>
2022-10-11 18:12:11 -0500

    Update overview.js

--- src/pages/about/overview/overview.js
+++ src/pages/about/overview/overview.js
@@ -5,8 +5,6 @@ import TamPrinciple from "../../../components/tam-principle/tam-principle";
 // to be updated with cms dynamically.
 let title = "Overview of Services";
 let imgSrc = "images/profile.png";
-let description =
-  "Tom Tam is a visionary servant leader, business executive, and coach with over 35 years of helping businesses, individuals, and families apply time-tested principles to solve their challenging problems. By working in the trenches as a problem solver with his clients in diverse settings, Tom saw the power of a principle-based approach to problem solving that are equally effective in business as well as for individuals and their families. He came up with an acronym using his last name TAM to highlight three important areas of focus for both businesses and individuals.";
 
 const Overview = () => (
   <div className="overview container-fluid">
@@ -17,7 +15,22 @@ const Overview = () => (
       <div className="col">
         <h2>{title}</h2>
         <br />
-        <p className="text-start">{description}</p>
+        <p className="text-start">
+          Tom Tam is a <span className="bolded">visionary servant leader</span>,
+          business executive, and coach with over 35 years of helping
+          businesses, individuals, and families apply time-tested principles to
+          solve their challenging problems. By working in the trenches as a
+          problem solver with his clients in diverse settings, Tom saw{" "}
+          <span className="bolded">
+            the power of a principle-based approach to problem solving
+          </span>{" "}
+          that are equally effective in business as well as for individuals and
+          their families. He came up with{" "}
+          <span className="bolded">
+            an acronym using his last name TAM to highlight three important
+            areas of focus for both businesses and individuals.
+          </span>
+        </p>
       </div>
     </div>
     <br />
