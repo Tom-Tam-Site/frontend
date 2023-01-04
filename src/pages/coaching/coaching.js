51e0883d9de2ecbebea94d8157d03517fb21f2f3
David Del Sol <David_Del_Sol_Castro@gap.com>
2023-01-04 17:51:02 -0600

    New changes

--- src/pages/coaching/coaching.js
+++ src/pages/coaching/coaching.js
@@ -1,11 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./coaching.css";
-import UnderConstruction from "../../components/under-construction/under-construction";
+import Contact from "../../components/contact/contact";
+
+import TamPrinciple from "../../components/tam-principle/tam-principle";
+
+import StrategyTam from "../../components/tam-principle/strategy-tam/strategy-tam";
+import RiskTam from "../../components/tam-principle/risk-tam/risk-tam";
+import TrainingTam from "../../components/tam-principle/training-tam/training-tam";
+
+let imgSrc = "images/profile.png";
 
 const Coaching = () => (
   <div className="coaching">
-    <UnderConstruction />
+    <div className="row text-center">
+      <Contact />
+      <div class="">
+        <div className="paragraph p-3 bg-light">
+          By working in the trenches as a problem solver with his clients in
+          diverse settings, Tom saw{" "}
+          <span className="bolded">
+            the power of a principle-based approach to problem solving
+          </span>{" "}
+          that is equally effective in business as well as for individuals and
+          their families.{" "}
+          <span className="bolded">
+            He created an acronym using his last name TAM to highlight three key
+            areas of focus for both businesses and individuals
+          </span>{" "}
+          for his consulting and coaching clients.
+        </div>
+      </div>
+      <div className="text-center mt-1">
+        <h2 class="title">TAM Principles</h2>
+        <StrategyTam />
+        <RiskTam />
+        <TrainingTam />
+      </div>
+    </div>
   </div>
 );
 
